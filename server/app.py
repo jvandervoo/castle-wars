a712d1c57d75b5692eb2859797098e09e05fa72d
Jasper <jasper.vandervoo@akqa.com>
2022-03-13 22:45:16 +1300

    Add simple echo websocket

--- server/app.py
+++ server/app.py
@@ -1,6 +1,23 @@
-from flask import Flask
-app = Flask(__name__)
+#!/usr/bin/env python
 
-@app.route('/')
-def hello_world():
-    return 'Hello alksdjlaksjd'
+import asyncio
+import websockets
+
+async def hello(websocket):
+    loop = True
+    while loop:
+        input = await websocket.recv()
+        greeting = f"Hello {input}!"
+        await websocket.send(greeting)
+        if (input == 'stop'):
+            await websocket.send("Stopping! Cya!")
+            loop = False
+
+async def main():
+    print("running websocket serve")
+    async with websockets.serve(hello, "0.0.0.0", 5000):
+        await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    print("running main")
+    asyncio.run(main())
