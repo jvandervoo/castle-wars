66e26b37ab1cdc18343c47b7b6d080af7cecfc6e
Jasper <jasper.vandervoo@akqa.com>
2022-03-13 21:27:03 +1300

    Server setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. cock cock
+          Edit <code>src/App.tsx</code> and save to reload. That was rude max
         </p>
         <a
           className="App-link"
