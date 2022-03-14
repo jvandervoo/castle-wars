f963530ac0f9f61328c79b4a2ff1311fc307936e
Jasper <jasper.vandervoo@akqa.com>
2022-03-14 20:29:12 +1300

    Store count in db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,39 @@
+import 'dotenv/config'
+import mongoose from 'mongoose';
 import { WebSocketServer } from 'ws';
+
+const uri = process.env.MONGODB_URI;
+
+const counterSchema = new mongoose.Schema({
+  count: Number
+})
+
+counterSchema.methods.increment = function increment() {
+  this.count++
+}
+
+const Counter = mongoose.model('counter', counterSchema);
+
+const counter = new Counter({ count: 0 });
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    wss.on('connection', function connection(ws) {
+      ws.on('message', async function message(data) {
+        console.log('received: %s', data);
+        data = JSON.parse(data.toString());
+        counter.increment();
+        await counter.save();
+        ws.send(`received: ${data.action}`);
+        ws.send(`count: ${counter.count}`);
+      });
+    });
+    console.log("MongoDB Connected - soccc opened");
+  })
+  .catch((err) => console.log(err));
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-    ws.send(`received: ${data}`);
-  });
-});
